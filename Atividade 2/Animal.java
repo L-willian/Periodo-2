d978089b32e59cf9f001edf8d1f9e78488c72f8d
Luiz Willian <luizwilliasoares930@gmail.com>
2026-09-13 20:56:25 -0300

    Adiciona método de brincar à classe Animal com lógica de felicidade e energia

--- Atividade 2/Animal.java
+++ Atividade 2/Animal.java
@@ -23,11 +23,26 @@ public class Animal {
             if (this.fome < 0) {
                 this.fome = 0;
             }
-            
+
             System.out.println(nome + "foi Alimentado com " + quantidade + " unidades de comida.");
 
         }
 
-        passarTempo();
+    }
+
+    public void brincar() {
+        if (this.energia <= 20) {
+            System.out.println("Animal com pouca energia, não pode brincar!");
+        } else {
+            this.felicidade += 10;
+            this.energia -= 20;
+
+            if (this.felicidade > 100 || this.energia >= 0) {
+                this.felicidade = 100;
+                this.energia = 0;
+
+                System.out.println(nome + " está feliz!");
+            }
+        }
     }
 }
