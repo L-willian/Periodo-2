d709ed52655b8d0ac457f4cbe26979850085eadf
Luiz Willian <luizwilliasoares930@gmail.com>
2026-09-13 22:38:13 -0300

    Adiciona métodos de dormir e passarTempo à classe Animal, atualiza lógica de felicidade e energia

--- Atividade 2/Animal.java
+++ Atividade 2/Animal.java
@@ -27,7 +27,7 @@ public class Animal {
             System.out.println(nome + "foi Alimentado com " + quantidade + " unidades de comida.");
 
         }
-
+        passarTempo();
     }
 
     public void brincar() {
@@ -44,5 +44,72 @@ public class Animal {
                 System.out.println(nome + " está feliz!");
             }
         }
+        passarTempo();
+    }
+
+    public void dormir() {
+        energia = 100;
+        fome += 25;
+
+        if (fome > 100) {
+            fome = 100;
+        }
+
+        System.out.println(nome + "Dormiu!!");
+
+        passarTempo();
+    }
+
+    protected void passarTempo() {
+        fome += 5;
+        energia -= 3;
+
+        if (fome <= 80) {
+            felicidade -= 10;
+        }
+        if (energia <= 10) {
+            felicidade -= 5;
+        }
+
+        if (fome > 100) {
+            fome = 100;
+        }
+        if (energia < 0) {
+            energia = 0;
+        }
+    }
+
+    public void getStatus() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Espécie: " + especie);
+        System.out.println("Fome: " + fome);
+        System.out.println("Energia: " + energia);
+        System.out.println("Felicidade: " + felicidade);
+
+        if(felicidade >= 70){
+            System.out.println("Seu bichinho está radiante de felicidade!!");
+        }
+        else if (felicidade >= 40 && felicidade <= 69){
+            System.out.println("Seu bichinho está bem, mas podia estar melhor!");
+        }
+        else {
+            System.out.println("Seu bichinho está triste, cuide melhor dele!!");
+        }
+    }
+
+    public int getFome() {
+        return fome;
+    }
+    
+    public int getEnergia() {
+        return energia;
+    }
+
+    public int getFelicidade() {
+        return felicidade;
+    }
+
+    public String getNome() {
+        return nome;
     }
 }
